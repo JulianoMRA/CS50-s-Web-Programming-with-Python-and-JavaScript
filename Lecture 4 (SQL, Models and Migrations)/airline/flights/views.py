d486eed4c191fbe58355eea37539bda20da8a93f
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-03 22:24:18 -0300

    adding passengers final

--- Lecture 4 (SQL, Models and Migrations)/airline/flights/views.py
+++ Lecture 4 (SQL, Models and Migrations)/airline/flights/views.py
@@ -14,12 +14,13 @@ def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
 
 def book(request, flight_id):
     if request.method == "POST":
         flight = Flight.objects.get(id=flight_id)
-        passenger = Passenger.object.get(id=int(request.POST["passenger"]))
+        passenger = Passenger.objects.get(id=int(request.POST["passenger"]))
         passenger.flights.add(flight)
-        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
