1e5b9aa48396f006fe6f11f560c42e894225da05
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-04 19:05:43 -0300

    Update counter.js

--- Lecture 5 (JavaScript)/counter.js
+++ Lecture 5 (JavaScript)/counter.js
@@ -4,12 +4,10 @@ function count() {
     counter++;
     heading = document.querySelector('h1');
     heading.innerHTML = counter;
-
-    if (counter % 10 == 0) {
-        alert(`Count is now ${counter}`);
-    }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').onclick = count;
+
+    setInterval(count, 1000);
 });
