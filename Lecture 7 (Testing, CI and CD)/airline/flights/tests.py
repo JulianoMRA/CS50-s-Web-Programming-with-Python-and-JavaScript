ab24deb090fe06477d44b3e33ca66c137a351327
Juliano Melo <julianomelorodriguesalencar@gmail.com>
2025-06-08 19:03:35 -0300

    using selenium for tests

--- Lecture 7 (Testing, CI and CD)/airline/flights/tests.py
+++ Lecture 7 (Testing, CI and CD)/airline/flights/tests.py
@@ -8,11 +8,11 @@ class FlightTestCase(TestCase):
 
     def setUp(self):
 
-        # Create airports.
+        # Airports
         a1 = Airport.objects.create(code="AAA", city="City A")
         a2 = Airport.objects.create(code="BBB", city="City B")
 
-        # Create flights.
+        # Flight
         Flight.objects.create(origin=a1, destination=a2, duration=100)
         Flight.objects.create(origin=a1, destination=a1, duration=200)
         Flight.objects.create(origin=a1, destination=a2, duration=-100)
